34a3a397b00f58a1c8801a474cb68fc5898cbf2b
cmx <caimx@ibuscloud.com>
2019-04-28 09:57:10 +0800

    init project

--- src/main/java/com/mds/matrix/MatrixApplication.java
+++ src/main/java/com/mds/matrix/MatrixApplication.java
@@ -3,10 +3,9 @@ package com.mds.matrix;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan({"com.mds.matrix.customer.mapper"})
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class MatrixApplication {
 
 	public static void main(String[] args) {
